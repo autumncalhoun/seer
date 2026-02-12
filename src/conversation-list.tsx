b9b9af6944821261680e44db7534ac112d30da51
Autumn Wright <autumn@autumncalhoun.com>
2026-02-12 11:51:08 -0500

    Add shadcn

---
+++ src/conversation-list.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { Conversation } from './api/external'
+
+export default function ConversationList({
+  conversations,
+}: {
+  conversations: Conversation[]
+}) {
+  if (conversations.length === 0) return <div>No conversations found</div>
+  return (
+    <div>
+      <h1>Conversations</h1>
+
+      <ul>
+        {conversations.map((conversation) => (
+          <li key={conversation.id}>{conversation.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
